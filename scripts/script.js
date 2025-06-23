6d667f3b9a56b4b8e5505fef5c2a3810d4f09c85
Lionel Lucky Aurelio <124338068+Lionellucky@users.noreply.github.com>
2025-06-23 20:56:47 +0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,39 @@ function topNavFunc() {
   }
 }
 
-funtion pageNav() {
-  //main fuction for page navigation
-  
+// 1) List out all your page filenames in order:
+const pages = [
+  'img/firstcomic/In_Another_Lifetime_Page_1.png',
+  'img/firstcomic/In_Another_Lifetime_Page_2.png',
+  'img/firstcomic/In_Another_Lifetime_Page_3.png',
+  // …add as many as you have (WIP)
+];
+
+// 2) Track where you are:
+let currentPage = 0;
+
+// 3) Grab the <img> once:
+const comicImage = document.getElementById('comicImage');
+
+// 4) A helper to show the right page:
+function updateImage() {
+  comicImage.src = pages[currentPage];
+}
+
+// 5) Next / Prev functions:
+function nextPage() {
+  if (currentPage < pages.length - 1) {
+    currentPage++;
+    updateImage();
+  }
+  // or to wrap around, use:
+  // currentPage = (currentPage + 1) % pages.length;
+}
+function prevPage() {
+  if (currentPage > 0) {
+    currentPage--;
+    updateImage();
+  }
+  // or to wrap: 
+  // currentPage = (currentPage - 1 + pages.length) % pages.length;
 }
