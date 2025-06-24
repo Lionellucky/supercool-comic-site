87dc79f388da850dd5b8226b153315957c2bb0d0
Lionel Lucky Aurelio <124338068+Lionellucky@users.noreply.github.com>
2025-06-24 19:02:48 +0700

    Update script.js added page nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,42 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function topNavFunc() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+window.onload = function () {
+  const pages = [
+    'img/firstcomic/In_Another_Lifetime_Page_1.png',
+    'img/firstcomic/In_Another_Lifetime_Page_2.png',
+    'img/firstcomic/In_Another_Lifetime_Page_3.png',
+    'img/firstcomic/In_Another_Lifetime_Page_4.png',
+    'img/firstcomic/In_Another_Lifetime_Page_5.png',
+    'img/firstcomic/In_Another_Lifetime_Page_6.png',
+    // add more as needed
+  ];
+
+  let currentPage = 0;
+  const comicImage = document.getElementById('comicImage');
+
+  function updateImage() {
+    comicImage.src = pages[currentPage];
   }
-}
 
-// 1) List out all your page filenames in order:
-const pages = [
-  'img/firstcomic/In_Another_Lifetime_Page_1.png',
-  'img/firstcomic/In_Another_Lifetime_Page_2.png',
-  'img/firstcomic/In_Another_Lifetime_Page_3.png',
-  // …add as many as you have (WIP)
-];
+  window.nextPage = function () {
+    if (currentPage < pages.length - 1) {
+      currentPage++;
+      updateImage();
+    }
+  };
 
-// 2) Track where you are:
-let currentPage = 0;
+  window.prevPage = function () {
+    if (currentPage > 0) {
+      currentPage--;
+      updateImage();
+    }
+  };
 
-// 3) Grab the <img> once:
-const comicImage = document.getElementById('comicImage');
-
-// 4) A helper to show the right page:
-function updateImage() {
-  comicImage.src = pages[currentPage];
-}
-
-// 5) Next / Prev functions:
-function nextPage() {
-  if (currentPage < pages.length - 1) {
-    currentPage++;
+  window.firstPage = function () {
+    currentPage = 0;
     updateImage();
-  }
-  // or to wrap around, use:
-  // currentPage = (currentPage + 1) % pages.length;
-}
-function prevPage() {
-  if (currentPage > 0) {
-    currentPage--;
+  };
+
+  window.lastPage = function () {
+    currentPage = pages.length - 1;
     updateImage();
-  }
-  // or to wrap: 
-  // currentPage = (currentPage - 1 + pages.length) % pages.length;
-}
+  };
+};
